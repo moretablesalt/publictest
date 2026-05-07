54e304903b4abd5b52883257db72c1c0965c40b7
Mark Tiu <mark.tiu@prudentialguarantee.com>
2026-05-07 13:25:28 +0800

    Fix spacing

--- src/main/java/com/moretablesalt/githubtest/GithubtestApplication.java
+++ src/main/java/com/moretablesalt/githubtest/GithubtestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GithubtestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GithubtestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GithubtestApplication.class, args);
+    }
 
 }
